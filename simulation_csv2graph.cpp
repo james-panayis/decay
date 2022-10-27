b466842de59699efb25b645fb7a18c82226e58df
James Panayis <james@jamespanayis.com>
2022-10-27 06:37:43 +0100

    Cleanup simulation_csv2graph.cpp by removing unnecessary pointers

--- simulation_csv2graph.cpp
+++ simulation_csv2graph.cpp
@@ -9,7 +9,7 @@ void roottest()
   auto canvas = std::make_unique<TCanvas>("canvas", "canvas", 3500, 2000);
 
   {
-    auto mg_p    = std::make_unique<TMultiGraph>();
+    TMultiGraph mg_p{};
 
     auto gr_p_K  = std::make_unique<TGraph>("cache/out.csv", "%lg,%lg");
     auto gr_p_pi = std::make_unique<TGraph>("cache/out.csv", "%*lg,%*lg,%lg,%lg");
@@ -17,16 +17,16 @@ void roottest()
     gr_p_K ->SetLineColor(kRed);
     gr_p_pi->SetLineColor(kBlue);
 
-    mg_p->Add(gr_p_K.release());
-    mg_p->Add(gr_p_pi.release());
+    mg_p.Add(gr_p_K.release());
+    mg_p.Add(gr_p_pi.release());
 
-    mg_p->Draw("a");
+    mg_p.Draw("a");
 
     canvas->SaveAs("cache/toy_simulation_p.png");
   }
 
   {
-    auto mg_pt    = std::make_unique<TMultiGraph>();
+    TMultiGraph mg_pt{};
 
     auto gr_pt_K  = std::make_unique<TGraph>("cache/out.csv", "%*lg,%*lg,%*lg,%*lg,%lg,%lg");
     auto gr_pt_pi = std::make_unique<TGraph>("cache/out.csv", "%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%lg,%lg");
@@ -34,16 +34,16 @@ void roottest()
     gr_pt_K ->SetLineColor(kRed);
     gr_pt_pi->SetLineColor(kBlue);
 
-    mg_pt->Add(gr_pt_K.release());
-    mg_pt->Add(gr_pt_pi.release());
+    mg_pt.Add(gr_pt_K.release());
+    mg_pt.Add(gr_pt_pi.release());
 
-    mg_pt->Draw("a");
+    mg_pt.Draw("a");
 
     canvas->SaveAs("cache/toy_simulation_pt.png");
   }
 
   {
-    auto mg_ip    = std::make_unique<TMultiGraph>();
+    TMultiGraph mg_ip{};
 
     auto gr_ip_K  = std::make_unique<TGraph>("cache/out.csv", "%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%lg,%lg");
     auto gr_ip_pi = std::make_unique<TGraph>("cache/out.csv", "%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%*lg,%lg,%lg");
@@ -51,10 +51,10 @@ void roottest()
     gr_ip_K ->SetLineColor(kRed);
     gr_ip_pi->SetLineColor(kBlue);
 
-    mg_ip->Add(gr_ip_K.release());
-    mg_ip->Add(gr_ip_pi.release());
+    mg_ip.Add(gr_ip_K.release());
+    mg_ip.Add(gr_ip_pi.release());
 
-    mg_ip->Draw("a");
+    mg_ip.Draw("a");
 
     canvas->SaveAs("cache/toy_simulation_ip.png");
   }

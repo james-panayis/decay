e7743adc78f5c86465f6834a57494c07c723a80a
James Panayis <james@jamespanayis.com>
2022-11-06 14:30:33 +0000

    Place new gaussians deterministically where most needed, instead of randomly, when adding them to a fit, thus iterate less times
    -position at largest residue
    -magnitude size of largest residue
    -width determined probalistically with fitting

--- fit.cpp
+++ fit.cpp
@@ -86,7 +86,7 @@ auto fit()
 
   std::mutex data_mutex;
 
-  auto canvas = std::make_unique<TCanvas>("canvas", "canvas", 2500, 1500);
+  auto canvas = std::make_unique<TCanvas>("canvas", "canvas", 3000, 1900);
 
   std::mutex canvas_mutex;
 
@@ -160,14 +160,12 @@ auto fit()
 
       std::vector<peak_t> peaks{};
 
-      double best_fit{std::numeric_limits<double>::infinity()};
-
       auto generate_new_peaks = [&]
       {
         auto new_peaks = peaks;
 
         //if (std::ssize(new_peaks) > 0 && (std::ssize(new_peaks) > 10 || std::bernoulli_distribution(0.9)(prng_)))
-        if (new_peaks.size() > 0 && std::bernoulli_distribution(0.9)(prng_))
+        if (new_peaks.size() > 1 && std::bernoulli_distribution(1.0 - std::pow(0.4, new_peaks.size()))(prng_))
         {
           // index of gaussian to alter
           const auto change_index = std::uniform_int_distribution(0ul, new_peaks.size() - 1)(prng_);
@@ -176,7 +174,7 @@ auto fit()
           {
             new_peaks.erase(new_peaks.begin() + static_cast<std::int64_t>(change_index));
           }
-          else if (std::bernoulli_distribution(0.05)(prng_))
+          else if (std::bernoulli_distribution(0.1)(prng_))
           {
             new_peaks[change_index].position += std::uniform_real_distribution(-span/100, span/100)(prng_);
           }
@@ -191,9 +189,68 @@ auto fit()
         }
         else
         {
+          /*
           new_peaks.emplace_back(std::uniform_real_distribution(min, max)(prng_), 
                                  std::uniform_real_distribution(0.0, static_cast<double>(vec.size()) * pow<2>(spread) / bucket_count)(prng_),
                                  std::uniform_real_distribution(2.5, span)(prng_));
+                                 */
+
+          const auto residuals = [&]
+          {
+            std::array<double, bucket_count> out;
+
+            for (std::uint32_t i = 0; i < out.size(); ++i)
+            {
+              out[i] = distribution[i];
+
+              for (const peak_t& peak : new_peaks)
+              {
+                const double offset = std::abs(peak.position - dist_vals[i]) / peak.width;
+
+                out[i] -= peak.magnitude * std::exp(-pow<2>(offset));
+              }
+            }
+
+            return out;
+          }();
+
+          const auto max_idx = static_cast<std::uint32_t>(std::max_element(residuals.begin(), residuals.end()) - residuals.begin());
+
+          const double position = dist_vals[max_idx];
+
+          const double magnitude = residuals[max_idx];
+
+          double best_fit = std::numeric_limits<double>::infinity();
+
+          double best_width = std::uniform_real_distribution(2.5, span)(prng_);
+
+          for (auto _ = 500; _--;)
+          {
+            const double width = best_width * std::uniform_real_distribution(9.0/10.0, 10.0/9.0)(prng_);
+
+            double fit{};
+
+            for (std::uint32_t i = 0; i < bucket_count; ++i)
+            {
+              const double offset = std::abs(position - dist_vals[i]) / width;
+
+              const double val = magnitude * std::exp(-pow<2>(offset));
+
+              fit += pow<2>(val - residuals[i]);
+            }
+
+            if (fit < best_fit)
+            {
+              best_fit = fit;
+
+              best_width = width;
+            }
+          }
+
+          if (best_fit == std::numeric_limits<double>::infinity())
+            fmt::print(fmt::emphasis::bold | fg(fmt::color::red), "ERROR: no change in best_fit when attempting to add a new curve\n");
+          else
+            new_peaks.emplace_back(position, magnitude, best_width);
         }
 
         return new_peaks;
@@ -220,7 +277,10 @@ auto fit()
         return fit;
       };
 
-      for (int i = 0; i < 200000; ++i)
+      double best_fit{std::numeric_limits<double>::infinity()};
+
+      for (int i = 0; i < 50000; ++i)
+      //for (int i = 0; i < 200000; ++i)
       //for (int i = 0; i < 5000; ++i)
       {
         const auto new_peaks = generate_new_peaks();

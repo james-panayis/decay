d59f83c7ada8aa2851e6b2d58d9f5ff6d9ae0795
James Panayis <james@jamespanayis.com>
2022-12-25 22:37:03 +0000

    Initial commit of custom c++ neural network to distinguish between signal and background events
    -standard feedforward neural network with logistic activation functions, using backpropagation and stochastic gradient descent
    -trained on 2018 up data, with simulated data within 300 of the Lb-mass considered to be signal, and with real data further than 300 from the Lb-mass considered to be background

---
+++ neural_net.cpp
@@ -0,0 +1,358 @@
+#include "root.hpp"
+
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TMultiGraph.h"
+#include "TLatex.h"
+//#include "ROOT/RDataFrame.hxx"
+
+#include "timeblit/random.hpp"
+
+#include <fmt/format.h>
+#include <fmt/compile.h>
+#include <fmt/color.h>
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <array>
+#include <algorithm>
+#include <fstream>
+#include <charconv>
+#include <iostream>
+#include <barrier>
+
+using namespace movency;
+
+// x to the power of non-negative integer P
+template<std::size_t P>
+[[nodiscard]] constexpr auto pow(auto x) -> decltype(x)
+{
+  if constexpr (P == 0)
+    return 1;
+  else if constexpr (P % 2 == 0)
+    return pow<P/2>(x) * pow<P/2>(x);
+  else
+    return x * pow<P/2>(x) * pow<P/2>(x);
+}
+
+
+using namespace std::literals;
+
+//constexpr auto variable_names = std::to_array<std::string_view>({"Lres_IPCHI2_OWNPV"sv, "h1_P"sv, "h1_PT"sv, "h2_P"sv, "h2_PT"sv, "Lres_FD_OWNPV"sv, "Jpsi_FD_OWNPV"sv, "Lres_TAUCHI2"sv, "Lb_IP_OWNPV"sv, "Jpsi_P"sv, "Jpsi_ENDVERTEX_CHI2"sv, "Lres_ENDVERTEX_CHI2"sv});
+constexpr auto variable_names = std::to_array<std::string_view>({"Lres_IPCHI2_OWNPV"sv, "h1_P"sv, "h1_PT"sv, "h2_P"sv, "h2_PT"sv, "Lres_FD_OWNPV"sv, "Jpsi_FD_OWNPV"sv, "Lres_TAUCHI2"sv, "Lb_IP_OWNPV"sv, "Jpsi_P"sv, "Jpsi_ENDVERTEX_CHI2"sv, "Lres_ENDVERTEX_CHI2"sv, "Lb_M"sv});
+//constexpr auto variable_names = std::to_array<std::string_view>({"h1_PT"sv, "h2_P"sv, "Lb_M"sv});
+
+constexpr std::size_t full_variable_count{variable_names.size()};
+constexpr std::size_t      variable_count{full_variable_count - 1};
+
+auto create_data()
+{
+  movency::root::file r_real{"../data/Lb2pKmm_mgUp_2018.root"};
+  movency::root::file r_simu{"../data/Lb2pKmm_sim_mgUp_2018.root"};
+
+  std::vector<std::array<double, full_variable_count + 1>> data{};
+
+  for (std::size_t variable_index = 0; variable_index < full_variable_count; ++variable_index)
+  {
+    auto variables{r_real.uncompress<double>(std::string(variable_names[variable_index]).c_str())};
+
+    fmt::print("reading {} real {} values\n", variables.size(), variable_names[variable_index]);
+
+    if (variables.size() > data.size())
+    {
+      if (data.size() == 0)
+        data.resize(variables.size());
+      else
+      {
+        fmt::print(fmt::emphasis::bold | fg(fmt::color::red), "ERROR: (real) {} {} {}\n", data.size(), variables.size(), variable_names[variable_index]);
+
+        std::exit(EXIT_FAILURE);
+      }
+    }
+
+    for (std::size_t i = 0; i < variables.size(); ++i)
+    {
+      data[i][variable_index] = variables[i];
+    }
+  }
+
+  std::size_t real_count = data.size();
+
+  for (auto& record : data)
+    record[full_variable_count] = 0;
+
+  for (std::size_t variable_index = 0; variable_index < full_variable_count; ++variable_index)
+  {
+    auto variables{r_simu.uncompress<double>(std::string(variable_names[variable_index]).c_str())};
+
+    fmt::print("reading {} simulated {} values\n", variables.size(), variable_names[variable_index]);
+
+    if (variables.size() + real_count > data.size())
+    {
+      if (data.size() == real_count)
+        data.resize(real_count + variables.size());
+      else
+      {
+        fmt::print(fmt::emphasis::bold | fg(fmt::color::red), "ERROR: (simu) {} {} {} {}\n", data.size(), variables.size(), variable_names[variable_index], real_count);
+
+        std::exit(EXIT_FAILURE);
+      }
+    }
+
+    for (std::size_t i = 0; i < variables.size(); ++i)
+    {
+      data[real_count + i][variable_index] = variables[i];
+    }
+  }
+
+  for (std::size_t i = real_count; i < data.size(); ++i)
+    data[i][full_variable_count] = 1;
+
+  std::erase_if(data, [](const auto e){ return (std::abs(e[full_variable_count - 1] - 5619.60) < 300.0 ) != e[full_variable_count]; });
+
+  real_count = 0;
+
+  for (const auto& e : data)
+    if (!e[full_variable_count])
+      ++real_count;
+
+  std::ranges::shuffle(data, movency::random::prng_);
+
+  for (std::size_t i = 0; i < variable_count; ++i)
+  {
+    const double div = std::ranges::max(data, {}, [=](const auto e){ return e[i]; })[i];
+
+    for (auto& e : data)
+      e[i] /= div;
+  }
+
+  fmt::print("created data. {} real and {} simulated events\n", real_count, data.size() - real_count);
+
+  return std::pair(data, static_cast<double>(real_count) / static_cast<double>(data.size()));
+}
+
+constexpr double logistic(const double val)
+{
+  return 1.0 / (1.0 + std::exp(-val)) - 0.5;
+}
+
+constexpr double derivative_logistic(const double val)
+{
+  const double exp = std::exp(val);
+
+  return exp / pow<2>(1.0 + exp);
+}
+
+constexpr double derivative_logistic_from_logistic(const double val)
+{
+  return (0.5 + val) * (0.5 - val);
+}
+
+int main()
+{
+  const auto [data, fraction_background] = create_data();
+
+  const auto fraction_signal = 1.0 - fraction_background;
+
+  constexpr std::size_t depth{5};
+
+  [[maybe_unused]] constexpr double dropout{0.5};
+
+  auto connections = []
+  {
+    std::array<std::array<std::array<double, variable_count>, variable_count>, depth - 1> out;
+    
+    for (auto& i : out)
+      for (auto& j : i)
+        for (auto& k : j)
+          k = 1.0 / variable_count * movency::random::fast(movency::random::uniform_distribution(-2.0, 2.0));
+
+    return out;
+  }();
+
+  bool print_next = true;
+
+  int cont{0};
+
+  constexpr std::size_t bucket_count{50};
+
+  std::array<std::array<double, 2>, bucket_count> histogram{};
+
+  auto run_iteration = [&]
+  {
+    std::size_t index = movency::random::fast(movency::random::uniform_distribution(std::size_t{0}, data.size() - 1));
+
+    std::array<std::array<double, variable_count>, depth> nodes;
+
+    for (std::size_t i = 0; i < variable_count; ++i)
+    {
+      //nodes[0][i] = logistic(data[index][i]);
+      nodes[0][i] = data[index][i];
+      //fmt::print("*{} ", nodes[0][i]);
+    }
+    //fmt::print("{}\n\n", data[index][full_variable_count]);
+
+    for (std::size_t i = 1; i < depth; ++i)
+      for (std::size_t j = 0; j < variable_count; ++j)
+      {
+        nodes[i][j] = 0;
+
+        for (std::size_t k = 0; k < variable_count; ++k)
+        {
+          nodes[i][j] += nodes[i - 1][k] * connections[i - 1][j][k];
+        }
+
+        nodes[i][j] = logistic(nodes[i][j]);
+        //fmt::print("***{} ", nodes[i][j]);
+      }
+
+    double score = 0;
+
+    for (std::size_t i = 0; i < variable_count; ++i)
+      score += nodes.back()[i];
+
+    score = logistic(score) + 0.5;
+    //score = score / variable_count;
+
+    // begin backpropagation of errors
+
+    const auto target = data[index][full_variable_count];
+
+    const auto bias = target ? fraction_background : fraction_signal;
+
+    histogram[static_cast<std::size_t>(score * bucket_count)][static_cast<std::size_t>(target)] += bias;
+    //fmt::print("s{}, stb{}\n", score, score * bucket_count);
+
+    const auto error = (target - score) * bias * derivative_logistic_from_logistic(score - 0.5);
+
+    decltype(nodes) errors;
+
+    for (std::size_t i = 0; i < variable_count; ++i)
+      errors.back()[i] = error / variable_count * derivative_logistic_from_logistic(nodes.back()[i]);
+
+    for (std::size_t i = depth - 2; i > 0; --i)
+      for (std::size_t j = 0; j < variable_count; ++j)
+      {
+        errors[i][j] = 0;
+
+        for (std::size_t k = 0; k < variable_count; ++k)
+          errors[i][j] += errors[i + 1][k] * connections[i][k][j];
+
+        errors[i][j] *= derivative_logistic_from_logistic(nodes[i][j]);
+        //errors[i][j] *= 4*derivative_logistic_from_logistic(nodes[i][j]);
+      }
+
+    // begin updating of weights
+
+    for (std::size_t i = 0; i < depth - 1; ++i)
+      for (std::size_t j = 0; j < variable_count; ++j)
+        for (std::size_t k = 0; k < variable_count; ++k)
+          connections[i][j][k] += 10 * errors[i + 1][j] * nodes[i][k];
+
+
+    //if (movency::random::fast(movency::random::uniform_distribution<bool>(0.00001)))
+    //if (movency::random::fast(movency::random::uniform_distribution<bool>(0.001)))
+    if (false)
+    {
+      if (data[index][full_variable_count] == print_next)
+      {
+        //fmt::print("                                                       ");
+        if (print_next)
+          fmt::print("\n");
+        else
+          fmt::print("       ");
+
+        fmt::print("score: {:>.7f}, {:>.7f}", score, target);
+
+        print_next = !print_next;
+      }
+    }
+
+    //if (movency::random::fast(movency::random::uniform_distribution<bool>(0.00001)))
+    //if(false)
+    if (!cont--)
+    {
+      fmt::print("\n\nscore: {}, {}\nnodes:\n", score, target);
+
+      for (std::size_t j = 0; j < variable_count; ++j)
+      {
+        for (std::size_t i = 0; i < depth; ++i)
+          fmt::print("{: f}  ", nodes[i][j]);
+          //fmt::print("{}  ", nodes[i][j]);
+
+        fmt::print("\n");
+      }
+
+      fmt::print("\n\nerrors:\n");
+
+      for (std::size_t j = 0; j < variable_count; ++j)
+      {
+        fmt::print(" unused    ");
+        for (std::size_t i = 1; i < depth; ++i)
+          fmt::print("{: f}  ", errors[i][j]);
+          //fmt::print("{}  ", nodes[i][j]);
+
+        fmt::print("\n");
+      }
+
+      fmt::print("\n");
+
+      //if(false)
+      for (std::size_t layer = 0; layer < depth - 1; ++layer)
+      {
+        fmt::print("\nconnections layer {}:\n", layer);
+        for (std::size_t i = 0; i < variable_count; ++i)
+        {
+          for (std::size_t j = 0; j < variable_count; ++j)
+          {
+            fmt::print("{: f}  ", connections[layer][i][j]);
+          }
+
+          fmt::print("\n");
+        }
+      }
+
+      const auto max_bucket = [&]
+      {
+        const auto temp = std::ranges::max(histogram, {}, [](auto b){ return b[0] + b[1]; });
+
+        return temp[0] + temp[1];
+      }();
+
+      for (const auto& bucket : histogram)
+      {
+        int i = 0;
+
+        for (; i < bucket[0] * 100 / max_bucket; i++)
+          fmt::print("0");
+
+        for (; i < (bucket[0] + bucket[1]) * 100 / max_bucket; i++)
+          fmt::print("1");
+
+        fmt::print(";\n");
+      }
+
+      for (auto& b : histogram)
+        for (auto& t : b)
+          t = 0;
+
+      fmt::print("\nInput rep count: ");
+      std::cin >> cont;
+
+      fmt::print("\n");
+    }
+
+    return score;
+  };
+
+  while(true)
+  {
+    run_iteration();
+  }
+
+  fmt::print("done\n");
+
+  return EXIT_SUCCESS;
+}
+

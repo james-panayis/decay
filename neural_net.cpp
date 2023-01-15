d1b39f9fc693e6f20ec4ca339561d2e67ea2cdfe
James Panayis <james@jamespanayis.com>
2023-01-15 21:59:12 +0000

    Improve threading functions to allow a thread-pool to be used instead of spawning then joining new threads everytime; use it to speedup ROC curve generation

--- neural_net.cpp
+++ neural_net.cpp
@@ -21,6 +21,7 @@
 #include <charconv>
 #include <iostream>
 #include <barrier>
+#include <functional>
 
 
 using namespace movency;
@@ -45,7 +46,7 @@ const std::size_t thread_count{std::max(1u, std::thread::hardware_concurrency() 
 // create a thread for each core and run the given function on each
 // the function must take 0 arguments, or a single std::uint32_t,
 // in which case the thread number [0, thread_count - 1]  will be passed
-void do_threaded(auto func)
+void do_threaded_without_pool(auto func)
 {
   std::vector<std::jthread> loop_threads{};
 
@@ -62,8 +63,68 @@ void do_threaded(auto func)
     }
 }
 
+
+std::vector<std::function<void()>> thread_functions(thread_count);
+
+std::array thread_blockers = 
+  ( []<std::size_t... I>(std::index_sequence<I...>)
+    { return std::array<std::binary_semaphore, sizeof...(I)>{{ ((void) I, std::binary_semaphore(0))... }}; }
+  )(std::make_index_sequence<100>{});
+
+std::array thread_completers = 
+  ( []<std::size_t... I>(std::index_sequence<I...>)
+    { return std::array<std::binary_semaphore, sizeof...(I)>{{ ((void) I, std::binary_semaphore(0))... }}; }
+  )(std::make_index_sequence<100>{});
+
+void thread_loop(const std::uint32_t thread_index)
+{
+  while (true)
+  {
+    thread_blockers[thread_index].acquire();
+
+    thread_functions[thread_index]();
+
+    thread_completers[thread_index].release();
+  }
+}
+
+std::vector<std::thread> threads = []
+{
+  std::vector<std::thread> out;
+
+  out.reserve(thread_count);
+
+  for (std::size_t thread_index = 0; thread_index < thread_count; ++thread_index)
+    out.emplace_back(thread_loop, thread_index);
+
+  return out;
+}();
+
+// run the given function on each thread in a thread pool
+// the function must take 0 arguments, or a single std::uint32_t,
+// in which case the thread number [0, thread_count - 1]  will be passed
+void do_threaded(auto func, const std::size_t thread_max = thread_count)
+{
+  for (std::uint32_t thread_index = 0; thread_index < thread_max; ++thread_index)
+  {
+    if constexpr (std::invocable<decltype(func)>)
+      thread_functions[thread_index] = func;
+    else
+    {
+      static_assert(std::invocable<decltype(func), std::uint32_t>, "test_repeat must be passed a function callable with 0 or 1 arguments");
+
+      thread_functions[thread_index] = std::bind(func, thread_index);
+    }
+
+    thread_blockers[thread_index].release();
+  }
+
+  for (std::uint32_t thread_index = 0; thread_index < thread_max; ++thread_index)
+    thread_completers[thread_index].acquire();
+}
+
 // call the given function on all values in the range [0, n - 1]
-// uses thread_count threads
+// uses the thread pool
 // provides no guarantees on execution order or which thread functions are executed on
 void loop_threaded(auto func, const std::size_t n)
 {
@@ -84,7 +145,10 @@ void loop_threaded(auto func, const std::size_t n)
     }
   };
 
-  do_threaded(do_next_index);
+  if (n < thread_count)
+    do_threaded(do_next_index, n);
+  else
+    do_threaded(do_next_index);
 }
 
 
@@ -293,8 +357,19 @@ void create_ROC_curve(const std::vector<T>& predictions, const std::string name,
     {
       const double cut = static_cast<double>(i) / point_count;
 
-      std::erase_if(signal_predictions,     [=](T v){ return v < cut; });
-      std::erase_if(background_predictions, [=](T v){ return v < cut; });
+      // maybe unparallelize? speedup is smaller than hoped for
+      //std::erase_if(signal_predictions,     [=](T v){ return v < cut; });
+      //std::erase_if(background_predictions, [=](T v){ return v < cut; });
+
+      auto cutdown = [&](std::size_t j)
+      {
+        if (j)
+          std::erase_if(signal_predictions,     [=](T v){ return v < cut; });
+        else
+          std::erase_if(background_predictions, [=](T v){ return v < cut; });
+      };
+
+      loop_threaded(cutdown, 2);
 
       const double next_signal    {static_cast<double>(    signal_predictions.size()) /     orig_signal_count};
       const double next_background{static_cast<double>(background_predictions.size()) / orig_background_count};
@@ -544,7 +619,7 @@ int main()
     }
   };
 
-  do_threaded(iterate);
+  do_threaded_without_pool(iterate);
 
   fmt::print("done\n");
 

800fbe242eee160efc8b4bb2e1054fbbc2136ed9
Callum White <callumwhite928@yahoo.co.uk>
2023-01-23 18:14:15 +0100

    Working version of RooFit_data.py

--- python/RooFit_data.py
+++ python/RooFit_data.py
@@ -12,33 +12,44 @@ plt.rc("font", **{"family": "serif"})  # , "serif": ["Roman"]})
 plt.rc("text", usetex=True)
 
 letter = "H"
-File= ROOT.TFile(f"../cache/D4J/Real_{letter}_D4J.root")
+File = ROOT.TFile(f"../cache/D4J/Real_{letter}_D4J.root")
 tree = File.Get("tree")
 
 lower_band = 5350.0
+#lower_band = 5010.0
 upper_band = 5850.0
+#upper_band = 6900.0
 lb_m = 5619.6
 cut = 0.981
 
 Lb_M = ROOT.RooRealVar("Lb_M", "Lb_M", lower_band, upper_band)
 xgb_output = ROOT.RooRealVar("xgb_output", "xgb_output", 0, 1)
+nsig = ROOT.RooRealVar("nsig", "nsig", 10000, 0, 1000000)
+nbkg = ROOT.RooRealVar("nbkg", "nbkg", 10000, 0, 1000000)
+nrand = ROOT.RooRealVar("nrand", "nrand", 10000, 0, 1000000)
 
 #sig
 mean = ROOT.RooRealVar("mean", "mean of gaussian", 5619.6)
-sigma = ROOT.RooRealVar("sigma", "width of gaussian", 1, 0.5,1000)
+sigma = ROOT.RooRealVar("sigma", "width of gaussian", 1, 0.1, 1000)
 sig = ROOT.RooGaussian("gauss", "gaussian PDF", Lb_M, mean, sigma)
 
 #bkg
-decay = ROOT.RooRealVar("decay", "decay constant", -3)
+decay = ROOT.RooRealVar("decay", "decay constant", 0, -0.05, 0.05)
 bkg = ROOT.RooExponential("exp", "exponential PDF", Lb_M, decay)
 
+#rand
+randmean = ROOT.RooRealVar("randmean", "mean of gaussian", 5510)
+randsigma = ROOT.RooRealVar("randsigma", "width of gaussian", 1, 0.5, 1000)
+rand = ROOT.RooGaussian("randgauss", "gaussian PDF", Lb_M, randmean, randsigma)
+
 #model
-model = ROOT.RooAddPdf("model", "", [sig,bkg], Lb_M)
+model = ROOT.RooAddPdf("model", "", [sig, bkg, rand], [nsig, nbkg, nrand])
+#model = ROOT.RooAddPdf("model", "", [sig,bkg], [nsig, nbkg])
 print("made model")
 
 #data
-data = model.generate({Lb_M}, 1000)
-print("generated data")
+#data = model.generate(ROOT>RooArgSet(Lb_M), 1000)
+#print("generated data")
 
 #frame
 Lb_Mframe = Lb_M.frame(Title="Fitting")
@@ -46,13 +57,20 @@ Lb_Mframe = Lb_M.frame(Title="Fitting")
 #dataset = makedata(tree, 0.981, lower_band, upper_band, 100)
 #data = RooDataHist("dataset", "dataset", {Lb_M}, Import=tree)
 
-data = ROOT.RooDataSet("data", "data", {Lb_M, xgb_output}, Import=tree, Cut=f"xgb_output>{cut}")
+data = ROOT.RooDataSet(
+    "data",
+    "data",
+    ROOT.RooArgSet(Lb_M, xgb_output),
+    Import=tree,
+    Cut=f"xgb_output>{cut}")
 
 model.fitTo(data)
 
 data.plotOn(Lb_Mframe)
 model.plotOn(Lb_Mframe)
 model.Print("t")
+
+Lb_Mframe.Draw()
 
 #xframe2 = x.frame(Title = "Gaussian pdf with data")
 #data.plotOn(xframe2)
@@ -94,7 +112,7 @@ model.Print("t")
 #    hist_data = ROOT.TH1F("hist_data", "hist_data", bins, xmin, xmax)
 #
 #    tree.fill("Lb_M >> hist_data", cut_string, "")
-#    
+#
 #    return hist_data
 #
 #
@@ -104,4 +122,3 @@ model.Print("t")
 #
 #
 #
-
